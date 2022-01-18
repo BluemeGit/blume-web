8dbd9505c25bde1e8076e5070cf3f00fe13aee40
이준호 <lhsljh123@ajou.ac.kr>
2022-01-19 02:07:20 +0900

    feat: add products page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,11 +1,58 @@
 
 import styled from '@emotion/styled';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import MobileHeader from '../../components/common/mobile/MobileHeader';
+import AdvertiseBox from '../../components/products/ads/AdvertiseBox';
+import Product from '../../components/products/Product';
+import { fetcher } from '../../fetch/fetcher';
+import useInput from '../../hooks/useInput';
+type ProductType = {
+    id: number,
+    name: string,
+    description: string,
+    imageUrl: string
+}
+type AdsType = {
+    title: string,
+    ImageURL: string,
+}
 export default function Products () {
+    const query = useInput('');
+    const [debounceQuery, setDebounceQuery] = useState<string>('');
+    const [timer, setTimer] = useState<any>(undefined); // 디바운싱 타이머
+
+    useEffect(() => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        const newTimer = setTimeout(async () => {
+            setDebounceQuery(query.value);
+        }, 800);
+        setTimer(newTimer);
+    }, [query.value]);
+    
+    const { data: products, error: productError } = useSWR(`/product/search?query=${debounceQuery}`, fetcher);
+    const { data: ads, error: adError } = useSWR('/product/ads', fetcher);
+
+    if (productError || adError) return <div>error!</div>
+    if (!ads) return <div>loading...</div>
+
+    
     return (
         <Container>
-
+            <MobileHeader/>
+            <Search {...query} placeholder={'사용하고 계신 제품명을 검색해보세요!'}/>
+            {products &&
+                <ProductContainer>
+                    {products.data.map((product: ProductType, id: number) => 
+                        <>
+                            <Product imageLink={product.imageUrl} id={product.id} title={product.name} content={product.description}/>
+                            {id % 4 === 3 && <AdvertiseBox src={ads.data[Math.floor(id / 4) % ads.data.length].ImageURL}/>}
+                        </>
+                    )}
+                </ProductContainer>
+            }       
         </Container>
     );
 }
@@ -14,3 +61,17 @@ const Container = styled.div`
 
 `;
 
+const Search = styled.input`
+    width: calc(100% - 2px);
+    box-sizing: border-box;
+    border: none;
+    height: 40px;
+    padding: 0px 20px;
+`;
+const ProductContainer = styled.div`
+    height: calc(100vh - 101px);
+    overflow-y: auto;
+
+    width: 100%;
+    border-top: 6px solid #F1F1F5;
+`
