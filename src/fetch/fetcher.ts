e95eb3b4a46ee4adacd11ff5e429d538f1222814
이준호 <lhsljh123@ajou.ac.kr>
2022-01-23 01:03:53 +0900

    fix: edit baseURL for backend

--- src/fetch/fetcher.ts
+++ src/fetch/fetcher.ts
@@ -1,3 +1,3 @@
 import axios from 'axios'
-const baseURL = 'http://118.67.134.32:3000';
+const baseURL = 'https://backend.maeee.co.kr';
 export const fetcher = (url: string) => axios.get(baseURL + url).then(res => res.data)
