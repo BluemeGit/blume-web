b406dabfbcf5e98cf7cec7fdd61ca21a3c399e23
taekywong Won <constwon111@gmail.com>
2022-04-03 21:39:44 +0900

    a

--- src/fetch/fetcher.ts
+++ src/fetch/fetcher.ts
@@ -1,4 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 // const baseURL = 'http://118.67.134.32:3000';
-const baseURL = 'https://backend.maeee.co.kr';
-export const fetcher = (url: string) => axios.get(baseURL + url).then(res => res.data)
+// const baseURL = 'https://backend.maeee.co.kr';
+const baseURL = "http://localhost:5000";
+export const fetcher = (url: string) =>
+  axios.get(baseURL + url).then((res) => res.data);
