146b4c858b70ed6f10d30979e5b28cfa8587196d
sungwon-097 <csw2757@gmail.com>
2022-06-27 10:27:26 +0900

    [fix] page views

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -10,169 +10,197 @@ import submit from "../../assets/common/submit.png";
 import { css } from "@emotion/react";
 
 export default function Comment() {
-  const [cat, setCat] = useState("orderByLatest");
-  const [commentList, setCommentList] = useState<any[]>([]);
-  const [description, setDescription] = useState("");
-  const [pageInfo, setPageInfo] = useState<any>({
-    pageNm: 1,
-    pageSize: 10,
-    pageEnd: false,
-  });
-
-  const user = useRecoilValue(userState);
-  console.log(user);
-  const params = useParams();
-  const onChangeInput = (e: any) => {
-    setDescription(e.target.value);
-  };
-  const onClickSubmit = () => {
-    if (!user?.accessToken) alert("로그인이 필요합니다.");
-    else {
-      poster(`/comment/${params.id}`, user?.accessToken, { description })
-        .then((result) => {
-          alert("완료되었습니다.");
-          window.location.reload();
-        })
-        .catch((err) => {
-          alert("실패했습니다.");
-        });
-    }
-  };
-
-  const onClickLike = (commentId: any) => {
-    if (!user?.accessToken) alert("로그인이 필요합니다.");
-    else {
-      putter(`/comment/like/${commentId}`, user?.accessToken).then((result) => {
-        console.log(result.data);
-      });
-      window.location.reload();
-    }
-  };
-
-  const onClickSort = (type: any = "orderByLatest") => {
-    if (user)
-      putter(`/comment/${params.id}?type=${type}`, user.accessToken, pageInfo)
-        .then((res) => {
-          if (res.data.length === 0 || res.data.length < 10)
-            setPageInfo({ ...pageInfo, pageEnd: true });
-          setCommentList((prev) => {
-            return [...prev, ...res.data];
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  };
-  const onClickReport = (commentId: any) => {
-    const body = { commentId };
-    poster(`/comment/report`, user.accessToken, body).then((result) => {
-      alert("신고 완료되었습니다");
+    const [cat, setCat] = useState("orderByLatest");
+    const [commentList, setCommentList] = useState<any[]>([]);
+    const [description, setDescription] = useState("");
+    const [pageInfo, setPageInfo] = useState<any>({
+        pageNm: 1,
+        pageSize: 10,
+        pageEnd: false,
     });
-  };
-  useEffect(() => {
-    onClickSort(cat);
-  }, [cat, pageInfo.pageNm]);
 
-  const onClickPaging = (type: any) => {
-    setPageInfo({ ...pageInfo, pageNm: pageInfo.pageNm + 1 });
-  };
+    const user = useRecoilValue(userState);
+    console.log(user);
+    const params = useParams();
+    const onChangeInput = (e: any) => {
+        setDescription(e.target.value);
+    };
+    const onClickSubmit = () => {
+        if (!user?.accessToken) alert("로그인이 필요합니다.");
+        else {
+            poster(`/comment/${params.id}`, user?.accessToken, { description })
+                .then((result) => {
+                    alert("완료되었습니다.");
+                    window.location.reload();
+                })
+                .catch((err) => {
+                    alert("실패했습니다.");
+                });
+        }
+    };
 
-  return (
-    <div>
-      <Container>
-        <CommentSquare
-          placeholder="제품에 대한 후기/의견/댓글을 자유롭게 써주세요"
-          onChange={onChangeInput}
-        />
-        <SubmitButton>
-          <img
-            id="logo"
-            src={submit}
-            onClick={onClickSubmit}
-            style={{ width: "18px", height: "22px" }}
-          />
-        </SubmitButton>
-      </Container>
-      {commentList.length > 0 && (
-        <section style={{ display: "flex", justifyContent: "center" }}>
-          <span
-            style={{
-              marginRight: "0.5rem",
-              color: cat === "orderByLatest" ? "rgb(30, 209, 84)" : "black",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              setCommentList([]);
-              setCat("orderByLatest");
-              setPageInfo({ ...pageInfo, pageNm: 1, pageEnd: false });
-            }}
-          >
-            최신순
-          </span>
-          <span
-            style={{
-              color: cat === "orderByRecommend" ? "rgb(30, 209, 84)" : "black",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              setCommentList([]);
-              setCat("orderByRecommend");
-              setPageInfo({ ...pageInfo, pageNm: 1, pageEnd: false });
-            }}
-          >
-            추천순
-          </span>
-        </section>
-      )}
-      <section>
-        {commentList.length > 0 &&
-          commentList?.map((item: any) => {
-            return (
-              <CommentBox
-                likeCnt={item.empathy}
-                commentId={item.id}
-                userId={item.userId}
-                nickname={item.nickname}
-                indate={item.indate}
-                description={item.description}
-                key={item.id}
-                onClickLike={onClickLike}
-                onClickReport={onClickReport}
-              />
+    const onClickLike = (commentId: any) => {
+        if (!user?.accessToken) alert("로그인이 필요합니다.");
+        else {
+            putter(`/comment/like/${commentId}`, user?.accessToken).then(
+                (result) => {
+                    console.log(result.data);
+                }
             );
-          })}
-      </section>
-      <Paging>
-        <ViewMore onClick={() => onClickPaging("next")}>
-          {!pageInfo.pageEnd && commentList.length > 0 && "더보기"}
-        </ViewMore>
-      </Paging>
-    </div>
-  );
+            window.location.reload();
+        }
+    };
+
+    const onClickSort = (type: any = "orderByLatest") => {
+        if (user)
+            putter(
+                `/comment/${params.id}?type=${type}`,
+                user.accessToken,
+                pageInfo
+            )
+                .then((res) => {
+                    if (res.data.length === 0 || res.data.length < 10)
+                        setPageInfo({ ...pageInfo, pageEnd: true });
+                    setCommentList((prev) => {
+                        return [...prev, ...res.data];
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+    };
+    const onClickReport = (commentId: any) => {
+        const body = { commentId };
+        poster(`/comment/report`, user.accessToken, body).then((result) => {
+            alert("신고 완료되었습니다");
+        });
+    };
+    useEffect(() => {
+        onClickSort(cat);
+    }, [cat, pageInfo.pageNm]);
+
+    const onClickPaging = (type: any) => {
+        setPageInfo({ ...pageInfo, pageNm: pageInfo.pageNm + 1 });
+    };
+
+    return (
+        <div>
+            <Container>
+                <CommentSquare
+                    placeholder="제품에 대한 후기/의견/댓글을 자유롭게 써주세요"
+                    onChange={onChangeInput}
+                />
+                <SubmitButton>
+                    <img
+                        id="logo"
+                        src={submit}
+                        onClick={onClickSubmit}
+                        style={{ width: "18px", height: "22px" }}
+                    />
+                </SubmitButton>
+            </Container>
+            <CommentContainer>
+                {commentList.length > 0 && (
+                    <section
+                        style={{ display: "flex", justifyContent: "center" }}
+                    >
+                        <span
+                            style={{
+                                marginRight: "0.5rem",
+                                color:
+                                    cat === "orderByLatest"
+                                        ? "rgb(30, 209, 84)"
+                                        : "black",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => {
+                                setCommentList([]);
+                                setCat("orderByLatest");
+                                setPageInfo({
+                                    ...pageInfo,
+                                    pageNm: 1,
+                                    pageEnd: false,
+                                });
+                            }}
+                        >
+                            최신순
+                        </span>
+                        <span
+                            style={{
+                                color:
+                                    cat === "orderByRecommend"
+                                        ? "rgb(30, 209, 84)"
+                                        : "black",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => {
+                                setCommentList([]);
+                                setCat("orderByRecommend");
+                                setPageInfo({
+                                    ...pageInfo,
+                                    pageNm: 1,
+                                    pageEnd: false,
+                                });
+                            }}
+                        >
+                            추천순
+                        </span>
+                    </section>
+                )}
+            </CommentContainer>
+            <section>
+                {commentList.length > 0 &&
+                    commentList?.map((item: any) => {
+                        return (
+                            <CommentBox
+                                likeCnt={item.empathy}
+                                commentId={item.id}
+                                userId={item.userId}
+                                nickname={item.nickname}
+                                indate={item.indate}
+                                description={item.description}
+                                key={item.id}
+                                onClickLike={onClickLike}
+                                onClickReport={onClickReport}
+                            />
+                        );
+                    })}
+            </section>
+            <Paging>
+                <ViewMore onClick={() => onClickPaging("next")}>
+                    {!pageInfo.pageEnd && commentList.length > 0 && "더보기"}
+                </ViewMore>
+            </Paging>
+        </div>
+    );
 }
 
 const Paging = styled.section`
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
 `;
 
 const ViewMore = styled.span`
-  cursor: pointer;
+    cursor: pointer;
 `;
 const Container = styled.div`
-  text-align: center;
-  width: 500px;
+    position: relative;
+    text-align: center;
+    padding: 20px 0 70px;
 `;
+const CommentContainer = styled.div``;
 const CommentSquare = styled.input`
-  width: 100%;
-  height: 3rem;
-  border-radius: 10px;
+    position: absolute;
+    width: 95%;
+    height: 3rem;
+    border-radius: 10px;
+    transform: translateX(-50%);
 `;
 const SubmitButton = styled.div`
-  position: absolute;
-  left: 90%;
-  padding: 0.5rem 0.2rem;
+    position: absolute;
+    top: 35%;
+    right: 4%;
 `;
 // .button {
 //     padding: 15px 25px;
--- src/components/comment/CommentBox.tsx
+++ src/components/comment/CommentBox.tsx
@@ -4,72 +4,77 @@ import React, { useEffect, useState } from "react";
 import { timeForToday } from "../../utils/timeForToday";
 
 export default function CommentBox({
-  userId,
-  nickname,
-  indate,
-  description,
-  onClickLike,
-  likeCnt,
-  commentId,
-  onClickReport,
+    userId,
+    nickname,
+    indate,
+    description,
+    onClickLike,
+    likeCnt,
+    commentId,
+    onClickReport,
 }) {
-  return (
-    <Conatiner>
-      <Wrap>
-        <Nick>{nickname}</Nick>
-        <InnerWrap>
-          <Btn
-            style={{ marginRight: "0.5rem" }}
-            onClick={() => onClickLike(commentId)}
-          >
-            추천 {likeCnt == 0 ? "" : likeCnt}
-          </Btn>
-          <Btn onClick={() => onClickReport(commentId)}>신고</Btn>
-        </InnerWrap>
-      </Wrap>
-      <CreatedAt>{timeForToday(indate)}</CreatedAt>
-      <Content>{description}</Content>
-      <FinishLine></FinishLine>
-    </Conatiner>
-  );
+    return (
+        <Conatiner>
+            <Wrap>
+                <Nick>{nickname}</Nick>
+                <InnerWrap>
+                    <Btn
+                        selected={likeCnt}
+                        style={{ marginRight: "2rem" }}
+                        onClick={() => onClickLike(commentId)}
+                    >
+                        추천 {likeCnt == 0 ? "" : likeCnt}
+                    </Btn>
+                    <Btn onClick={() => onClickReport(commentId)}>신고</Btn>
+                </InnerWrap>
+            </Wrap>
+            <CreatedAt>{timeForToday(indate)}</CreatedAt>
+            <Content>{description}</Content>
+            <FinishLine></FinishLine>
+        </Conatiner>
+    );
 }
 
 const Conatiner = styled.div`
-  padding: 10px;
+    padding: 10px;
 `;
 
 const Nick = styled.div`
-  font-weight: bold;
-  font-size: 0.9rem;
+    font-weight: bold;
+    font-size: 0.9rem;
 `;
 
 const CreatedAt = styled.div`
-  font-size: 0.5rem;
+    font-size: 0.5rem;
 `;
 
 const Content = styled.div`
-  font-size: 0.8rem;
+    font-size: 0.8rem;
 `;
 
 const FinishLine = styled.hr`
-  margin-bottom: 1rem;
-  border: none;
-  height: 0.5px;
-  background-color: #eeeeee;
-  /* background-color: red; */
-  /* color: red; */
-  /* border-color: #090909; */
-  border-bottom: 0.5px;
+    margin-bottom: 1rem;
+    border: none;
+    height: 0.5px;
+    background-color: #eeeeee;
+    /* background-color: red; */
+    /* color: red; */
+    /* border-color: #090909; */
+    border-bottom: 0.5px;
 `;
 
 const Wrap = styled.div`
-  display: flex;
-  justify-content: space-between;
+    position: relative;
+    justify-content: space-between;
 `;
 
 const InnerWrap = styled.div``;
 
-const Btn = styled.span`
-  font-size: 0.9rem;
-  cursor: pointer;
+const Btn = styled.p<{ selected?: boolean }>`
+    position: absolute;
+    top: 10%;
+    right: 0%;
+    color: ${(props) => (props.selected ? "#1ED154" : "#000000")};
+    font-size: 0.9rem;
+    cursor: pointer;
 `;
